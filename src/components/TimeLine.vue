5cfd6a353e7fd13b32496ed3d7d84e05358848de
Christian Griffin <c.griffin@vehikl.com>
2019-10-11 14:40:34 -0400

    remove unneeded generated files and add TimeLine vue component

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="timeline">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TimeLine extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
