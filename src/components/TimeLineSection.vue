9468e956ac644fbec2fb4d6ccd09b323f9261b90
Christian Griffin <christian.griffin@live.com>
2019-10-20 21:56:17 -0400

    move timeline fade variables into variables file

--- src/components/TimeLineSection.vue
+++ src/components/TimeLineSection.vue
@@ -25,9 +25,6 @@
 <style scoped lang="scss">
     @import './../assets/scss/variables';
 
-    $fadeHeight: 10px;
-    $gradientHeight: 80%;
-
     .timeline-wrapper {
         z-index: 4;
         position: relative;
@@ -37,16 +34,16 @@
     .top-fade {
         position: relative;
         z-index: 5;
-        height: $fadeHeight;
-        margin-bottom: -$fadeHeight;
-        background: -webkit-linear-gradient(top, rgba($backgroundColor, 1) #{99.5% - $gradientHeight}, rgba($backgroundColor, 0) 99.5%);
+        height: $timelineFadeHeight;
+        margin-bottom: -$timelineFadeHeight;
+        background: -webkit-linear-gradient(top, rgba($backgroundColor, 1) #{99.5% - $timelineGradientHeight}, rgba($backgroundColor, 0) 99.5%);
     }
 
     .bottom-fade {
         position: relative;
         z-index: 5;
-        height: $fadeHeight;
-        margin-top: -$fadeHeight;
-        background: -webkit-linear-gradient(top, rgba($backgroundColor, 0) 0.5%, rgba($backgroundColor, 1) #{0.5% + $gradientHeight});
+        height: $timelineFadeHeight;
+        margin-top: -$timelineFadeHeight;
+        background: -webkit-linear-gradient(top, rgba($backgroundColor, 0) 0.5%, rgba($backgroundColor, 1) #{0.5% + $timelineGradientHeight});
     }
 </style>
