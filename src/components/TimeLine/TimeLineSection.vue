04a498288cec69f0e496a2b25dda5fa1b0248ac2
Christian Griffin <c.griffin@vehikl.com>
2019-10-21 10:16:49 -0400

    make background image parallax

--- src/components/TimeLine/TimeLineSection.vue
+++ src/components/TimeLine/TimeLineSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="timeline">
         <h2>Timeline of Events</h2>
         <time-line/>
     </section>
@@ -8,13 +8,18 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import TimeLine from '@/components/TimeLine/TimeLine.vue';
+    import { jarallax } from 'jarallax';
 
     @Component({
         components: { TimeLine },
     })
 
     export default class TimeLineSection extends Vue {
-
+        private mounted() {
+            jarallax(document.querySelector('section.timeline'), {
+                speed: 0.6
+            });
+        }
     }
 </script>
 
@@ -27,10 +32,10 @@
     }
 
     section {
+        padding-left: 0;
+        padding-right: 0;
         background-image: url('./../../assets/images/triangles.jpg');
         background-repeat: repeat;
         background-size: 60%;
-        padding-left: 0;
-        padding-right: 0;
     }
 </style>
