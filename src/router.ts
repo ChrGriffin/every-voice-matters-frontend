a6dc55271f474bc9d33917977ea00b1716cd1d36
Christian Griffin <christian.griffin@live.com>
2019-10-24 22:39:22 -0400

    set page title tags from router

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ import Contact from './views/Contact.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const appRouter = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
@@ -13,11 +13,53 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home,
+            meta: {
+                title: 'Every Voice Matters'
+            }
         },
         {
             path: '/contact',
             name: 'contact',
             component: Contact,
+            meta: {
+                title: 'Contact | Every Voice Matters'
+            }
         },
     ],
 });
+
+// shameless copy+paste from https://alligator.io/vuejs/vue-router-modify-head/
+// hence why every typehint is `any`, idk wtf it does ¯\_(ツ)_/¯
+appRouter.beforeEach((to: any, from: any, next: any) => {
+    const nearestWithTitle = to.matched.slice().reverse().find((r: any) => r.meta && r.meta.title);
+    const nearestWithMeta = to.matched.slice().reverse().find((r: any) => r.meta && r.meta.metaTags);
+    if(nearestWithTitle) {
+        document.title = nearestWithTitle.meta.title;
+    }
+
+    Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map((el) => {
+        if(el.parentNode !== null) {
+            el.parentNode.removeChild(el);
+        }
+    });
+
+    if(!nearestWithMeta) {
+        return next();
+    }
+
+    nearestWithMeta.meta.metaTags.map((tagDef: any) => {
+        const tag = document.createElement('meta');
+
+        Object.keys(tagDef).forEach(key => {
+            tag.setAttribute(key, tagDef[key]);
+        });
+
+        tag.setAttribute('data-vue-router-controlled', '');
+        return tag;
+    })
+        .forEach((tag: any) => document.head.appendChild(tag));
+
+    next();
+});
+
+export default appRouter;
